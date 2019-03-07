fe6a6ff43dba9b927bb05f7fc5707f989d0d3896
magicbe <magicbe.tw@gmail.com>
2019-03-07 11:42:37 +0800

    update CH08 package demo

--- CH08/ModuleDemo.py
+++ CH08/ModuleDemo.py
@@ -1,5 +1,5 @@
-import cal.calculate
-import cal.supercalculate
+from cal.calculate import add
+from cal.supercalculate import addAdvanced
 
-print(cal.calculate.add(3, 5))
-print(cal.supercalculate.addAdvanced(3, 4, 5, 6, 7))
+print(add(3, 5))
+print(addAdvanced(3, 4, 5, 6, 7))
